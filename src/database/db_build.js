83ae2b82d50acc5f29f405cf8bf37223da0788b8
YousefQwasmeh <151037@ppu.edu.ps>
2019-07-24 17:11:13 +0300

    resolve comment

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -4,10 +4,6 @@ const dbConnection = require("./db_connection.js");
 const sqlPath = path.join(__dirname, "db_build.sql");
 const sql = fs.readFileSync(sqlPath).toString();
 
-const runDbBuild = cb => dbConnection.query(sql, cb);
-module.exports = runDbBuild; 
-
-
 const runDbBuild = cb => {
 dbConnection.query(sql, cb);
 };
